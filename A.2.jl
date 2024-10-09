26e9dfd1089c3a8992b4c031e167571db03ade57
kjl-macbook <keith.ja.lee@gmail.com>
2024-10-08 21:40:56 -0400

    cleanup before publishing

--- A.2.jl
+++ A.2.jl
@@ -17,66 +17,6 @@ begin
     lmax = 30
 
     model = generate_npod(nlegs, [0, 0, 10])
-end
 
-#sample
-n_total_samples = 500
-cull_factor = 25
-
-samples_grid = grid_sampler(n_total_samples, 2, bounds)
-xsamples = getindex.(samples_grid, 1)
-zsamples = getindex.(samples_grid, 2)
-
-fl = Float64[]
-lengths_violated = Bool[]
-
-for (x, z) in zip(xsamples, zsamples)
-    pos = [x, 0, z]
-
-    model = generate_npod(nlegs, pos)
-    f = Asap.axial_force.(model.elements)
-    l = length.(model.elements)
-
-    push!(fl, sum(abs.(f) .* l))
-
-    if any(l .< lmin) || any(l .> lmax)
-        push!(lengths_violated, true)
-    else
-        push!(lengths_violated, false)
-    end
-end
-
-#assign a colour depending on if the length constraitns were violated
-pt_colors = [violation ? :red : :blue for violation in lengths_violated]
-
-#note that the following is equivalent:
-pt_colors = []
-for violation in lengths_violated
-    if violation == true
-        push!(pt_colors, :red)
-    else
-        push!(pt_colors, :blue)
-    end
-end
-
-fl_best = minimum(fl)
-i_valid = findall(fl .<= cull_factor*fl_best)
-i_invalid = findall(fl .> cull_factor*fl_best)
-
-#plot
-begin
-    fig = Figure()
-    ax = Axis3(
-        fig[1,1],
-        xlabel = "X [m]",
-        ylabel = "Z [m]",
-        zlabel = "∑|FL|"
-    )
-
-    scatter!(
-        xsamples[i_valid], zsamples[i_valid], fl[i_valid],
-        color = pt_colors[i_valid]
-    )
-
-    fig
+    n_total_samples = 1000
 end
